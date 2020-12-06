52c8704d2144376233aaa6da508661696ddf98d3
karanbhandariweb <bhandarikaran212@gmail.com>
2020-12-06 18:06:21 +0530

    Updates

---
+++ precache-manifest.0a35a7ec9900662ef3a22c785c43dd0c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b1819061033175535ba42985cc48ba",
+    "url": "/personalportfolio/index.html"
+  },
+  {
+    "revision": "ae38f82489347f0e1ae7",
+    "url": "/personalportfolio/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "f4df477d1c0e727b08fb",
+    "url": "/personalportfolio/static/css/main.67368668.chunk.css"
+  },
+  {
+    "revision": "ae38f82489347f0e1ae7",
+    "url": "/personalportfolio/static/js/2.e557a8df.chunk.js"
+  },
+  {
+    "revision": "54e8c4f782a1df2882087d61fdec9005",
+    "url": "/personalportfolio/static/js/2.e557a8df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4df477d1c0e727b08fb",
+    "url": "/personalportfolio/static/js/main.eb53d251.chunk.js"
+  },
+  {
+    "revision": "06f3b3ad412882a3e8fd",
+    "url": "/personalportfolio/static/js/runtime-main.0cc1b2c6.js"
+  },
+  {
+    "revision": "4538042dea58ee0e1711aa732ebb7522",
+    "url": "/personalportfolio/static/media/cms.4538042d.png"
+  },
+  {
+    "revision": "fc9e98ccf0832cf09f06cf46e70214ee",
+    "url": "/personalportfolio/static/media/cmss.fc9e98cc.png"
+  },
+  {
+    "revision": "bf6ea0408ec460b47be4a3cd827e4688",
+    "url": "/personalportfolio/static/media/flipm.bf6ea040.png"
+  },
+  {
+    "revision": "f90bcff33824944fcaa2ef3b220be412",
+    "url": "/personalportfolio/static/media/lgf.f90bcff3.png"
+  },
+  {
+    "revision": "322f49a7b3e9f24a2b1d81873dadfc5e",
+    "url": "/personalportfolio/static/media/picart.322f49a7.png"
+  },
+  {
+    "revision": "bbdc22c58bb21e5e8babb653f8c32ff8",
+    "url": "/personalportfolio/static/media/res.bbdc22c5.png"
+  },
+  {
+    "revision": "dc36d381e0f63bcd7c8f696988908f16",
+    "url": "/personalportfolio/static/media/vcrd.dc36d381.png"
+  },
+  {
+    "revision": "722670701f5da95384607c645edde171",
+    "url": "/personalportfolio/static/media/wclk.72267070.png"
+  }
+]);
